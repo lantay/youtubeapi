61fd044026552771133c5c59e49cae90fd1cf324
Mason <gmlantay@gmail.com>
2017-10-12 14:59:26 +0800

    notes + non-envsh api key added

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// ح˚௰˚づ  ○ code we write in other files won't mess 
+//with our file unless we import it
+// means we have to import react everywhere ح˚௰˚づ
+import React from 'react';
+//ح˚௰˚づ  react dom is what connects react to the dom
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyDdTfaxaWIUIqbdxGgbFt5e0kfjrG2HQdQ'
+//-Create a new componenet. Should produce some html
+//-this is like a factory that creates a componenet
+//non es6
+  // const App = function () {
+  //   return <div>Hi!</div>;
+  // }
+
+const app = () => {
+  return <div>Hi!</div>;
+}
+
+  // ح˚௰˚づ• webpack and babel together make bundle.js
+//ح˚௰˚づ • component is a set of js functions that produces html
+// javascript modules
+
+
+//-Take component's generated HTML
+// and put it on the page //(in the dom)
+  //do by putting it in whatever the root of the app is
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+            //????????things don't know????????
+                //difference between class and instance
